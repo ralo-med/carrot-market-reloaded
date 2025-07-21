09977d6f6e60939ddc3fd32f1f57a803fb6decaf
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-22 08:08:07 +0900

    #Email login

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,5 +1,32 @@
 "use server";
 
+import bcrypt from "bcrypt";
+import db from "@/lib/db";
+import { z } from "zod";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return Boolean(user);
+};
+
+const formSchema = z.object({
+  email: z
+
+    .email()
+    .toLowerCase()
+    .refine(checkEmailExists, "An account with this email does not exist."),
+  password: z.string().min(1, "Password is required"),
+});
+
 interface FormState {
   fieldErrors?: {
     email?: string[];
@@ -7,15 +34,40 @@ interface FormState {
   };
 }
 
-export async function handleForm(
-  prevState: FormState | null
-): Promise<FormState> {
-  console.log(prevState);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  return {
-    fieldErrors: {
-      email: ["wrong email"],
-      password: ["wrong password"],
-    },
+export async function logIn(prevState: FormState | null, formData: FormData) {
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
   };
+  const result = await formSchema.spa(data);
+  if (!result.success) {
+    return z.flattenError(result.error);
+  } else {
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+    const ok = await bcrypt.compare(
+      result.data.password,
+      user!.password ?? "xxxx"
+    );
+    if (ok) {
+      const session = await getSession();
+      session.id = user!.id;
+      await session.save();
+      redirect("/profile");
+    } else {
+      return {
+        fieldErrors: {
+          password: ["Wrong password."],
+          email: [],
+        },
+      };
+    }
+  }
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,10 +5,20 @@ import Button from "@/components/btn";
 import Input from "@/components/input";
 import SocialLogin from "@/components/social-login";
 import { useActionState } from "react";
-import { handleForm } from "./actions";
+import { logIn } from "./actions";
+
+interface FormState {
+  fieldErrors?: {
+    email?: string[];
+    password?: string[];
+  };
+}
 
 export default function LogIn() {
-  const [state, action] = useActionState(handleForm, null);
+  const [state, action] = useActionState<FormState | null, FormData>(
+    logIn,
+    null
+  );
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
