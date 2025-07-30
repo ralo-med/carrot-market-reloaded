93833348e98625b91067d92c37133f97911fdf4b
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-30 09:18:56 +0900

    #일단 버그 하나 고치고

--- app/(tabs)/layout.tsx
+++ app/(tabs)/layout.tsx
@@ -1,10 +1,17 @@
 import TabBar from "@/components/tab-bar";
+import { Suspense } from "react";
 
 export default function TabLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       {children}
-      <TabBar />
+      <Suspense
+        fallback={
+          <div className="fixed bottom-0 w-full max-w-screen-sm mx-auto grid grid-cols-5 bg-neutral-800 border-neutral-600 border-t px-5 py-3 *:text-white h-16"></div>
+        }
+      >
+        <TabBar />
+      </Suspense>
     </div>
   );
 }
