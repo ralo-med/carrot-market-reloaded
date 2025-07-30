ed6fbfe17d65fa56d84ae45807d2a7386488e804
Ralo <74175604+ralo-med@users.noreply.github.com>
2025-07-30 10:19:08 +0900

    #그냥 라우트 바꿔버림 고치기 힘드네

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -47,7 +47,7 @@ export default async function Products() {
     <div>
       <ProductList initialProducts={initialProducts} />
       <Link
-        href="/products/add"
+        href="/add"
         className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
       >
         <PlusIcon className="size-10" />
